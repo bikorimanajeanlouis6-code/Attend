0493d40bf4c8dfb563d316e4b9265f97ae966513
bikorimana louis <bikorimanajeanlouis6@gmail.com>
2026-08-11 10:17:58 +0200

    Add interpolation and barchart option

--- Web/obj/Debug/net10.0/Web.AssemblyInfo.cs
+++ Web/obj/Debug/net10.0/Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc612db105a37e0a25d9bb9ff3288d467e8a679f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0792c723dac531e91b449a19ccbf9bb3c4716f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
